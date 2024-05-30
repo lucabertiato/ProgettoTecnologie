1469306c896c50778cd524acfd910261b2b2d55c
Luca Bertiato <lucabertiato@gmail.com>
2024-05-30 11:41:59 +0200

    Avvio struttura per spring boot

---
+++ codice/snapfolio/src/main/java/com/snapfolio/snapfolio/SnapfolioApplication.java
@@ -0,0 +1,13 @@
+package com.snapfolio.snapfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapfolioApplication.class, args);
+	}
+
+}
---
+++ codice/snapfolio/src/test/java/com/snapfolio/snapfolio/SnapfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snapfolio.snapfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
